d1d4ed10d3d8f672868a0ac520816d110dd7a54b
rbulgarelli <rosebulgarelli1@gmail.com>
2014-11-30 10:37:39 -0500

    Lab 10: Stored Procedures
    By: Rosemary Bulgarelli
    11/30/2014
    I tried all over thanksgiving and was unable to  figure out the optional
    challenge... darn recursion

---
+++ Lab 10.sql
@@ -0,0 +1,45 @@
+﻿-- Rosemary Bulgarelli -- 
+-- 11/18/2014 --
+-- Lab 10: Stored Procedure --
+ 
+-- 1. function PreReqsFor --
+create or replace function PreReqsFor(int, REFCURSOR) returns refcursor as
+$$
+declare
+   course_num int      :=$1;
+   resultset REFCURSOR :=$2;
+begin
+   open resultset for
+      select prereqnum
+      from prerequisites
+      where coursenum = course_num;
+   return resultset;
+end;
+$$
+language plpgsql;
+
+-- Test for function PreReqsFor --	
+select PreReqsFor(499, 'results');
+Fetch all from results;
+
+
+-- 2. function IsPreReqFor --
+create or replace function isPreReqFor(int, REFCURSOR) returns refcursor as
+$$
+declare
+  course_num int      :=$1;  
+  resultset REFCURSOR :=$2;
+begin
+  open resultset for
+     select coursenum
+     from prerequisites
+     where prereqnum = course_num;
+  return resultset;
+end;
+$$
+language plpgsql;
+
+-- Test for function PreReqsFor --	
+select PreReqsFor(221, 'results');
+Fetch all from results;
+
